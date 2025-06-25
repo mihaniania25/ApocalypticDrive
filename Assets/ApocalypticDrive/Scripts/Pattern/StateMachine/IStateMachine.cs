9cade58f47af3c95a468cf0eedfcb68f35182303
Mykhailo Blagun <me.shine.factory@gmail.com>
2025-06-25 22:50:54 +0300

    [DEV] state machine pattern

---
+++ Assets/ApocalypticDrive/Scripts/Pattern/StateMachine/IStateMachine.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace MeShineFactory.ApocalypticDrive.Pattern.StateMachine
+{
+    public interface IStateMachine<DataType> where DataType : IStateData
+    {
+        UniTask Setup();
+        UniTask RunState(DataType stateData);
+        UniTask Dispose();
+    }
+}
